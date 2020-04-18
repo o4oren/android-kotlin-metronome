cab1146dc50ac4d1a83df23d567f809e8abbd506
Oren Geva <o4oren@gmail.com>
2020-04-18 17:30:35 +0300

    BeatsView functionality

--- app/src/main/java/geva/oren/android_kotlin_metronome/views/BeatsView.kt
+++ app/src/main/java/geva/oren/android_kotlin_metronome/views/BeatsView.kt
@@ -2,17 +2,34 @@ package geva.oren.android_kotlin_metronome.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import geva.oren.android_kotlin_metronome.R
 
 class BeatsView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
     var beats = 4
+    val emptyCircle = ContextCompat.getDrawable(context, R.drawable.beat_circle_empty)
+    val fullCircle = ContextCompat.getDrawable(context, R.drawable.beat_circle_full)
+    var marginParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
     init {
         orientation = HORIZONTAL
-        TODO("add children")
+        marginParams.setMargins(5, 5, 5, 5)
+        createBeats()
+    }
+
+    private fun createBeats() {
+        for (i in 0 until beats) {
+            var imageView = ImageView(context)
+            val drawableToSet = if (i==0) fullCircle else emptyCircle
+            imageView.setImageDrawable(drawableToSet)
+            imageView.layoutParams = marginParams
+            addView(imageView)
+        }
     }
 
     fun inc(): Int {
