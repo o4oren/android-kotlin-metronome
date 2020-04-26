9c9554ac635b4c8bb2d30568dd53ba3971cec56d
Oren Geva <o4oren@gmail.com>
2020-04-26 20:16:45 +0300

    animation

--- app/src/main/java/geva/oren/android_kotlin_metronome/fragments/MechanicalMetronomeFragment.kt
+++ app/src/main/java/geva/oren/android_kotlin_metronome/fragments/MechanicalMetronomeFragment.kt
@@ -1,11 +1,16 @@
 package geva.oren.android_kotlin_metronome.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.RotateAnimation
+import androidx.fragment.app.Fragment
 import geva.oren.android_kotlin_metronome.R
+import kotlinx.android.synthetic.main.mechanical_metronome_fragment.*
+
 
 /**
  * A simple [Fragment] subclass.
@@ -24,5 +29,16 @@ class MechanicalMetronomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.mechanical_metronome_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val an = RotateAnimation(-30.0f, 30.0f, Animation.RELATIVE_TO_PARENT, 0.5f, Animation.RELATIVE_TO_SELF, 0.7f)
+        an.duration = 1500 // duration in ms
+        an.repeatCount = -1 // -1 = infinite repeated
+        an.repeatMode = Animation.REVERSE // reverses each repeat
+        an.fillAfter = true // keep rotation after animation
+
+        metronomeArmView.animation = an
     }
 }
